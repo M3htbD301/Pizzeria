53a369ecc005d0019e482edb9718c18d90d7a3de
M3htbD301 <81451074+M3htbD301@users.noreply.github.com>
2021-06-09 10:21:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var firstName="Mehtab ";
+console.log(firstName);
+var lastName="Deol";
+console.log(lastName);
+var fullName=firstName.concat(lastName);
+console.log(fullName);
+var greeting=fullName.replace("Deol","says hi");
+console.log(greeting);
